f1d2cf8e33b577b850ce829d3f30c5f46bb001b3
r00dan <rudenko.rbi@gmail.com>
2021-12-31 01:08:27 +0300

    created Post component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,17 +1,40 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import s from '../styles/Navigation.module.css';
 
 export function Navigation() {
+  const [visible, setVisible] = useState<boolean>(false);
   return (
-    <ul className={s.navigation}>
-      <li>
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
-      </li>
-      <li>
-        <a href="https://github.com/r00dan" target="_blank" rel="noreferrer">Gihub</a>
-      </li>
-    </ul>
+    <div className={s.navigation}>
+      <ul>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li>
+          <a href="https://github.com/r00dan" target="_blank" rel="noreferrer">Gihub</a>
+        </li>
+      </ul>
+      <div>
+        Menu
+      </div>
+      {
+        visible && (
+          <div className={s.mobile_nav}>
+             <ul>
+              <li>
+                <Link href="/blog">
+                  <a>Blog</a>
+                </Link>
+              </li>
+              <li>
+                <a href="https://github.com/r00dan" target="_blank" rel="noreferrer">Gihub</a>
+              </li>
+            </ul>
+          </div>
+        )
+      }
+    </div>
   )
 }
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import s from '../../styles/Post.module.css';
 
 export default function Post() {
   const router = useRouter();
-  const { id } = router.query;
+  const { id, image } = router.query;
+  useEffect(() => {
+    document.title = `Post #${id}`
+  }, []);
   return (
-    <div>
-      Post #{id}
+    <div className={s.post}>
+      <h2>Some good title</h2>
+      {
+        image && (
+          <img src={image} alt="post-image" />
+        )
+      }
+      <div className={s.text}>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores velit ab pariatur quaerat nesciunt sit aut sequi totam, quo, vel quisquam, eligendi possimus similique. Modi provident similique deserunt excepturi commodi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores velit ab pariatur quaerat nesciunt sit aut sequi totam, quo, vel quisquam, eligendi possimus similique. Modi provident similique deserunt excepturi commodi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores velit ab pariatur quaerat nesciunt sit aut sequi totam, quo, vel quisquam, eligendi possimus similique. Modi provident similique deserunt excepturi commodi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores velit ab pariatur quaerat nesciunt sit aut sequi totam, quo, vel quisquam, eligendi possimus similique. Modi provident similique deserunt excepturi commodi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores velit ab pariatur quaerat nesciunt sit aut sequi totam, quo, vel quisquam, eligendi possimus similique. Modi provident similique deserunt excepturi commodi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores velit ab pariatur quaerat nesciunt sit aut sequi totam, quo, vel quisquam, eligendi possimus similique. Modi provident similique deserunt excepturi commodi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores velit ab pariatur quaerat nesciunt sit aut sequi totam, quo, vel quisquam, eligendi possimus similique. Modi provident similique deserunt excepturi commodi.</p>
+      </div>
+
+      <div className={s.date}>
+        {new Date().toLocaleDateString('ru', { year: 'numeric', month: 'long', day: 'numeric' })}
+      </div>
     </div>
   )
 }
